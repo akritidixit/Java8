b3c1d38ff334847be1f79d786b5534cd5a7412f4
Akrati Dixit <akritid82@gmail.com>
2019-03-11 03:23:06 -0700

    Add files via upload

--- DefaultMethodDemo.java
+++ DefaultMethodDemo.java
@@ -1,9 +1,33 @@
+interface TestInterface
+{
+	//abstract void
+	public void square (int a);
+	
+	//default method
+	default void show()
+	{
+		System.out.println("Default method executed");
+	}
+	//static method
+	static void show1() {
+		System.out.println("Static method Executed");
+	}
+}
+public class DefaultMethodDemo implements TestInterface{
 
-public class DefaultMethodDemo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void square(int a)  {
+		System.out.println(a*a);
+		}
+	
+	public static void main(String [] args) {
+		DefaultMethodDemo d = new DefaultMethodDemo();
+		d.square(4);
+		
+		//default method executed
+		d.show();
+		
+		//static method executed
+		TestInterface.show1();
 	}
 
 }
